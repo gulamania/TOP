2f27aa31111dc15df8f73fc4a764d5e53e03b804
Nia <58320188+gulamania@users.noreply.github.com>
2024-02-04 15:57:25 -0500

    finished 04

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let outArray = []
+    for (let element of array){
+        if (!args.includes(element)){
+            outArray.push(element)
+        }
+    }
+    return outArray
 };
+
+removeFromArray([1, 2, 3, 4], 3)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
