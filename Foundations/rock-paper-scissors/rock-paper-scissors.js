da97b752df64b98cfdb8533d063806f20043b02d
Nia <58320188+gulamania@users.noreply.github.com>
2024-02-04 14:50:56 -0500

    Finished Rock-paper-scissors

--- Foundations/rock-paper-scissors/rock-paper-scissors.js
+++ Foundations/rock-paper-scissors/rock-paper-scissors.js
@@ -24,16 +24,47 @@ function evaluateRound(computerChoice, playerChoice){
     switch (playerChoice){
         case "Rock":
             if (computerChoice == "Paper"){return "You Lose! Paper beats Rock"}
-            return "You Win! Rock beats Scissors"
+            return "You Win! Rock beats Scissors";
         case "Paper":
             if (computerChoice == "Scissors"){return "You Lose! Scissors beats Paper"}
-            return "You Win! Paper beats Rock"
+            return "You Win! Paper beats Rock";
         case "Scissors":
             if (computerChoice == "Rock"){return "You Lose! Rock beats Scissors"}
-            return "You Win! Scissors beats Paper"
+            return "You Win! Scissors beats Paper";
     }
 }
 
-let playerChoice = cleanPlayerChoice(prompt("rock paper or scissors?"));
-let computerChoice = getComputerChoice();
-console.log(evaluateRound(computerChoice, playerChoice))
+function playRound(computerScore, playerScore){
+    let playerChoice = cleanPlayerChoice(prompt("rock paper or scissors?"));
+    let computerChoice = getComputerChoice();
+    let result =  evaluateRound(computerChoice, playerChoice);
+    console.log(result)
+    if (result.includes("Win")){
+        playerScore =+ 1;
+    } else if (result.includes("Lose")){
+        computerScore =+ 1;
+    }
+    console.log("The computer has " + computerScore + " points while you have " + playerScore + " points") 
+    return [computerScore, playerScore]
+}
+
+function playGame(){
+    // No loops bcs odin project doesn't want you to use them TT
+    let computerScore = 0;
+    let playerScore = 0;
+    let result = playRound(computerScore, playerScore)
+    computerScore = result[0]
+    playerScore = result[1]
+    result = playRound(computerScore, playerScore)
+    computerScore = result[0]
+    playerScore = result[1]
+    result = playRound(computerScore, playerScore)
+    computerScore = result[0]
+    playerScore = result[1]
+    result = playRound(computerScore, playerScore)
+    computerScore = result[0]
+    playerScore = result[1]
+    result = playRound(computerScore, playerScore)
+}
+
+playGame()
