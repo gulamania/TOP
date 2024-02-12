71ff741f893ec8ca5aee45e5e1ce3c7ad1ea637a
Nia <58320188+gulamania@users.noreply.github.com>
2024-02-12 15:18:17 -0500

    done etchasketch

--- Foundations/etch-a-sketch/script.js
+++ Foundations/etch-a-sketch/script.js
@@ -1,20 +1,42 @@
 const bigContainer = document.querySelector('.bigContainer');
+const resetButton = document.querySelector('button')
 
-function renewCanvas(bigContainer) {
-    for (var i = 0; i < 16; i++){
-        var rowContainer = document.createElement('div');
-        rowContainer.className = "rowContainer";
-        bigContainer.appendChild(rowContainer)
-        for (var i = 0; i < 16; i++){
-            var cell = document.createElement('div');
-            cell.className = "cell";
-            cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = 'grey';
-            });
-            rowContainer.appendChild(cell);
-        }
-    }
+function addColorListener(cell){
+    cell.addEventListener('mouseover', () => {
+        var currentColor = Number(cell.style.
+            backgroundColor.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2'));
+        var newColor = Math.max(currentColor - 25.6, 0);
+        cell.style.backgroundColor = 'rgb('+ newColor + ',' + newColor + ',' + newColor + ')';
+    });
 };
 
-renewCanvas(bigContainer);
+function renewCanvas(bigContainer, n) {
+    var edgeLength = 500/(n) - 4;
+    var cellCount = n * n
+    for (var i = 0; i < cellCount; i++){
+        var cell = document.createElement('div');
+        cell.className = "cell";
+        cell.style.width = edgeLength + "px"; 
+        cell.style.height = edgeLength + "px";
+        cell.style.backgroundColor = '#ffffff'
+        bigContainer.appendChild(cell);
+    };
+    const children = bigContainer.children;
+    for (var index = 0; index < cellCount; index++){
+        addColorListener(children[index]);
+    };
+};
+
+resetButton.addEventListener('click', () => {
+    bigContainer.innerHTML = ''
+    const n = prompt("how many cells do you want on each edge?")
+    renewCanvas(bigContainer, n);
+});
+
+renewCanvas(bigContainer, 16);
+
+
+
+
+
 
