11568e6507abdc939e50e4be0ce27b9cadec0de5
Nia <58320188+gulamania@users.noreply.github.com>
2024-02-13 10:17:00 -0500

    finished up js exercises

--- javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    const titles = bookList.map((book) => book['title']);
+    return titles
 };
 
 // Do not edit below this line
--- javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    for (person in peopleArray){//add age variables to the objects
+        var birthYear = peopleArray[person]['yearOfBirth'];
+        if ('yearOfDeath' in peopleArray[person]){ //dead case
+            var deathYear = peopleArray[person]['yearOfDeath'];
+            peopleArray[person]['age'] =  deathYear - birthYear;
+        } else { //alive case
+            var curYear = new Date().getFullYear();
+            peopleArray[person]['age'] =  curYear - birthYear;
+        }
+    }
 
+    const getOlderPerson = function(person1, person2){
+        if (person1['age'] < person2['age']){
+            return person2
+        } else {return person1}
+    }
+
+    let minAge = {age: 0};
+    const oldestPerson = peopleArray.reduce(getOlderPerson, minAge);
+    return oldestPerson
 };
 
 // Do not edit below this line
