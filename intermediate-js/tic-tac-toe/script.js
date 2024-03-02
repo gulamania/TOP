79a2541243562c1c693b2522fa11fae0234bcf99
Nia <58320188+gulamania@users.noreply.github.com>
2024-03-02 09:05:06 -0500

    tictactow Frame complete!

--- intermediate-js/tic-tac-toe/script.js
+++ intermediate-js/tic-tac-toe/script.js
@@ -1,4 +1,4 @@
-const Gameboard = (function(){
+const Gameboard = (function() {
     //define board state
     const rows = 3;
     const cols = 3;
@@ -7,8 +7,8 @@ const Gameboard = (function(){
     //populate board with cells
     for (var i = 0; i < rows; i++){
         board[i] = [];
-        for (var j = 0; i < cols; j++){
-            board[i].push(Cell()) // Cell needs to be defined to hold three values
+        for (var j = 0; j < cols; j++){
+            board[i].push(Cell()); // Cell needs to be defined to hold three values
         }
     }
 
@@ -17,13 +17,24 @@ const Gameboard = (function(){
 
     //Modify board state from player action
     const play = (x, y, playerToken) => {
-        if (board[y][x].getValue() != "") return; // Failed Action 
+        if (board[y][x].getValue() != "") {return}; // Failed Action 
         
         board[y][x].addToken(playerToken); // If valid place token
     };
 
+    //print board state to console
+    const printBoard = () =>{
+        var str = "Board State:";
+        for (var i = 0; i < rows; i++){
+            for (var j = 0; j < cols; j++){
+                str = str + String(board[i][j].getValue());
+            }
+        }
+        return str
+    };
+
     //And Out
-    return {getBoard, play};
+    return {getBoard, play, printBoard};
 })();
 
 function Cell(){
@@ -44,11 +55,9 @@ function Cell(){
 }
 
 function makePlayer(name, id){
-    return {name, id}
+    return {name, id};
 }
 
-
-/*
 const GameController = (function(){
     playerOne = makePlayer("PlayerOne", 1);
     playerTwo = makePlayer("PlayerTwo", 2);
@@ -62,27 +71,58 @@ const GameController = (function(){
         currPlayer = currPlayer === players[0] ? players[1] : players[0]
     };
 
+    const valueToSym = (value) => {
+        switch (value){
+            case 0:
+                return "e";
+            case 1:
+                return "X";
+            case 2:
+                return "O";
+        }
+    }
+
     //update board
-    const printBoard = () => {
-        const boardNode = document.querySelector('table');
-        for (var i = 0; i < rows; i++){
-            for (var j = 0; i < cols; j++){
-                var currCell = boardNode.childNodes[i].childNodes[j];
-                switch (currCell.getValue()){
-                    case 0:
-                        currSym = "e"
-                    case 1:
-                        currSym = "X"
-                    case 2:
-                        currSym = "O"
-                }
+    const boardStateToDOM = () => {
+        const boardNode = document.querySelector('table').childNodes[1]; //selects table body
+        var currSym = ""
+        for (var i = 0; i < 3; i++){
+            for (var j = 0; j < 3; j++){
+                var value = Gameboard.getBoard()[i][j].getValue();
+                var currCell = boardNode.childNodes[i*2].childNodes[j*2+1]; //there is a pattern to the text nodes, so the i*2 is needed for traversal 
+                var currSym = valueToSym(value);
                 currCell.innerHTML = currSym;
             }
         }
     }
 
-    //play a round
+    //what exactly happens when someone plays a symbol
+    const playSymbol = (evt) => {
+        //mk vars
+        var node = evt['target']
+        var colid = node.id[0];
+        var rowid = node.id[1];
+        
+        Gameboard.play(rowid, colid, currPlayer["id"]);
 
-    return {switchPlayer, printBoard}
+        boardStateToDOM();
+        
+        switchPlayer();
+
+        //check for a win
+        
+        node.removeEventListener('click', playSymbol)
+        //node.removeEventListener('hover', showSymbol)
+    }
+
+    //Add Event Listeners to cells
+    const listenersToDOM = () => {
+        const cellNodes = document.querySelectorAll('td');
+        cellNodes.forEach(node => node.addEventListener('click', playSymbol));
+    }
+
+    //And Out
+    return {listenersToDOM}
     
-})(); */
+})();
+GameController.listenersToDOM();
